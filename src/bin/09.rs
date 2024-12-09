64d8ddf29e0031ee330951767dfc1d4d70313412
a-nickol <a.nickol@trust-nickol.de>
2024-12-09 16:22:42 +0100

    day 9

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -51,8 +51,80 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(sum)
 }
 
+#[derive(Clone, Copy, Debug)]
+struct File {
+    len: u64,
+    pos: u64,
+    idx: Option<u64>,
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut disk = vec![];
+    let mut pos = 0;
+    let mut idx_vec = vec![];
+    for (i, c) in input.trim().chars().enumerate() {
+        let occupied = i % 2 == 0;
+        let id = (i / 2) as u64;
+        let len = c.to_string().parse::<u64>().unwrap();
+        if len > 0 {
+            disk.push(File {
+                len,
+                pos,
+                idx: if occupied { Some(id) } else { None },
+            });
+            if occupied {
+                idx_vec.push(id);
+            }
+        }
+        pos += len;
+    }
+
+    loop {
+        let file_idx = idx_vec.pop();
+        if file_idx.is_none() {
+            break;
+        }
+
+        let file_idx = file_idx.unwrap();
+        let j = disk.iter().position(|f| f.idx == Some(file_idx)).unwrap();
+
+        let len = disk[j].len;
+        let mut i = 0;
+        loop {
+            if i == disk.len() - 1 || (disk[i].idx.is_none() && disk[i].len >= len) {
+                break;
+            }
+            i += 1;
+        }
+
+        if disk[i].pos < disk[j].pos && disk[i].len >= len && disk[i].idx.is_none() {
+            let d = disk[i];
+            disk[i] = disk[j];
+            disk[i].pos = d.pos;
+            disk[j].idx = None;
+            if d.len > disk[i].len {
+                disk.insert(
+                    i + 1,
+                    File {
+                        len: d.len - disk[i].len,
+                        pos: disk[i].pos + disk[i].len,
+                        idx: None,
+                    },
+                );
+            }
+        }
+    }
+
+    let mut sum = 0_u64;
+    for file in disk.iter() {
+        for i in file.pos..(file.pos + file.len) {
+            if let Some(idx) = file.idx {
+                sum += idx * i;
+            }
+        }
+    }
+
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -68,6 +140,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2858));
     }
 }
